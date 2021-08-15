52d30d2450167652a91bbeabb1bb9db7088318b3
Eduardo Rodrigues da Silva <silva.rodriguesedu@icloud.com>
2021-08-15 18:32:24 -0300

    Alter createJobs.groovy file

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/rodrigues-edu/spring-boot-api-example-devops.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
+    }
+}
